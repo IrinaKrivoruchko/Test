49de763c29828c214371990755cbbfb6af15759b
IrinaKrivoruchko <sokurira@gmail.com>
2024-03-05 10:49:23 +0200

    Update task_2

--- task_2.py
+++ task_2.py
@@ -5,29 +5,40 @@ user_name = input('Hello! What is your name?\n')
 number = random.randint(1, 40)
 
 print(f'Well, {user_name}, I am thinking of a number between 1 and 40.')
-count_of_attempts = 1
 
-while count_of_attempts <= 10:
+finish_answer = 'y'
 
-    try:
-        guess_number = int(input('Take a guess! Enter your number: '))
+while finish_answer == 'y':
+    count_of_attempts = 1
+    found = False
+    while count_of_attempts <= 10:
 
-        if guess_number == number:
-            print(f'Good job, {user_name}! You are a winner!')
-            break
+        try:
+            guess_number = int(input('Take a guess! Enter your number: '))
 
-        elif 1 <= guess_number <= 40 and guess_number < number:
-            print('Your number is less.')
+            if guess_number == number:
+                print(f'Good job, {user_name}! You are a winner!')
+                found = True
+                break
 
-        elif 1 <= guess_number <= 40 and guess_number > number:
-            print('Your number is more.')
+            elif 1 <= guess_number <= 40 and guess_number < number:
+                print('Your number is less.')
 
-        elif not 1 <= guess_number <= 40:
-            print(f"Your number is not in the range 1 to 40. Try again:)")
+            elif 1 <= guess_number <= 40 and guess_number > number:
+                print('Your number is more.')
 
-    except ValueError as exception:
-        print("Enter only integer numbers please!")
+            elif not 1 <= guess_number <= 40:
+                print(f'Your number is not in the range 1 to 40. Try again:)')
 
-    count_of_attempts += 1
+        except ValueError as exception:
+            print("Enter only integer numbers please!")
 
-print("You had only 10 attempts... Game over!")
+        count_of_attempts += 1
+
+    if not found:
+        print('You had only 10 attempts... Game over!')
+
+
+    finish_answer = input('Do you want to play again? y/n\n')
+
+print('Thank you for game')
